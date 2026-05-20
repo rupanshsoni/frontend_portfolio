bcf7390921acc24ceea9631cee687ec02bcce426
Rupansh Soni <soni.rupansh@yahoo.in>
2026-05-21 01:01:00 +0530

    fix: correct creator's name in README and add built-by section in footer

---
+++ src/components/LangSwitch.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Button from "./Button.vue";
+</script>
+
+<template>
+  <Button
+    variant="border"
+    size="sm"
+    data-cursor="circle-white"
+    data-hoversound="hover"
+    >EN</Button
+  >
+</template>
